739e580dc7d356c2b4c4b344c95eeaad7ec75a9a
nickonyi <nickoluoch1130@gmail.com>
2026-07-12 19:15:00 +0300

    feat: add database seeding script to populate usernames table

---
+++ db/populatedb.js
@@ -0,0 +1,29 @@
+import { Client } from "pg";
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS usernames(
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+username VARCHAR(255)
+);
+
+INSERT INTO usernames(username)
+VALUES 
+('Bryan'),
+('Odin'),
+('Damon');
+`;
+
+const main = async () => {
+  console.log("Seeding...");
+  const client = new Client({
+    connectionString:
+      "postgresql://phatommobb:admin123@localhost:5432/top_users",
+  });
+
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("Done!");
+};
+
+main();
